f5201d6f0daf7539c35f427c3bb0e004d06b1f15
Nikola <krlenix@gmail.com>
2021-11-05 13:06:05 +0100

    Multifunkcionalni masazer v1

--- resources/views/lander/purplerelax/rs/multifunkcionalnimasazer.blade.php
+++ resources/views/lander/purplerelax/rs/multifunkcionalnimasazer.blade.php
@@ -16,43 +16,43 @@
 <section class="block2">
     <div class="wrap header__wrap">
         <div class="">
-            <h1><span>Массажер</span> для спины и шеи </h1>
+            <h1><span>MASAŽER</span> ZA LEĐA I VRAT </h1>
             <div class="sale">
-                скидка
-                <p>-50%</p>
+                popust
+                <p>-40%</p>
             </div>
             <ul class="plus">
                 <li>
-                    <p>Помогает снять стресс</p>
+                    <p>Pomaže u oslobađanju od stresa</p>
                 </li>
                 <li>
-                    <p>Снимает боль в мышцах</p>
+                    <p>Ublažava bolove u mišićima</p>
                 </li>
                 <li>
-                    <p>Помогает расслабиться</p>
+                    <p>Pomaže da se potpuno opustite</p>
                 </li>
             </ul>
             <div class="price clearfix">
                 <div class="old-cost">
-                    <span>Старая цена:</span>
+                    <span>Stara cena:</span>
                     <p>
                         {{ $prices[1]['originalPrice'] }}
                         RSD
                     </p>
                 </div>
                 <div class="new-cost">
-                    <span>Новая цена:</span>
+                    <span>Nova cena:</span>
                     <p>
                         {{ $prices[1]['amount'] }}
                         RSD
                     </p>
                 </div>
             </div>
-            <a href="#buy" class="button-m">Заказать со скидкой</a>
+            <a href="#buy" class="button-m">Poručite sada</a>
         </div>
         <div class="video__wrap" style="width: 300px;">
             <video muted="muted" width="300px" height="auto" loop="loop" preload="auto" playsinline="" webkit-playsinline="" autoplay="autoplay" controls="">
-                <source src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/1.mp4" type="video/mp4">
+                <source src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/1_1.mp4" type="video/mp4">
             </video>
         </div>
     </div>
@@ -62,49 +62,49 @@
         <ul class="result">
             <li>
                 <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/res1.png" alt="">
-                <p><b>Массаж шеи</b></p>
+                <p><b>Masaža vrata</b></p>
             </li>
             <li>
                 <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/res2.png" alt="">
-                <p><b>Массаж спины</b></p>
+                <p><b>Masaža leđa</b></p>
             </li>
             <li>
                 <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/res3.png" alt="">
-                <p><b>Массаж талии</b></p>
+                <p><b>Masaža struka</b></p>
             </li>
             <li>
                 <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/res4.png" alt="">
-                <p><b>Массаж ног</b></p>
+                <p><b>Masaža stopala</b></p>
             </li>
         </ul>
     </div>
 </section>
 <section class="block5">
     <div class="wrap">
-        <h2 class="title t2">Массажер для спины и шеи</h2>
+        <h2 class="title t2">MASAŽER ZA LEĐA I VRAT</h2>
         <ul class="sostav">
             <li>
                 <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/sos1.jpg" alt="">
-                <h4>Массажная головка</h4>
+                <h4>Masaža glave</h4>
             </li>
             <li>
                 <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/sos2.jpg" alt="">
-                <h4>Дышащая нейлоновая сетчатая ткань</h4>
+                <h4>Prozračna mrežasta tkanina</h4>
             </li>
             <li>
                 <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/sos3.jpg" alt="">
-                <h4>Высококачественная кожа PU</h4>
+                <h4>Visokokvalitetna koža</h4>
             </li>
             <li>
                 <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/sos4.jpg" alt="">
-                <h4>3C сертифицированный источник питания</h4>
+                <h4>3C sertifikovano napajanje</h4>
             </li>
         </ul>
     </div>
 </section>
 <section class="block7" style="background:white;">
     <div class="wrap">
-        <h2>Массажер для спины и шеи</h2>
+        <h2>MASAŽER ZA LEĐA I VRAT</h2>
         <div class="goods">
             <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/sl1.jpg" alt="">
             <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/sl2.jpg" alt="">
@@ -114,27 +114,30 @@
 </section>
 <section class="block9">
     <div class="wrap">
-        <h2 class="title">отзывы покупателей</h2>
+        <h2 class="title">UTISCI KUPACA</h2>
         <div class="reviews">
             <div>
                 <div class="rev">
                     <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/rev1.jpg" alt="">
-                    <h3>Александра Щербакова</h3>
-                    <p class="center-justified">Массажер очень классный. Недавно купила у вас на сайте. Теперь пользуюсь с огроменным удовольствием) Покупкой довольна!</p>
+                    <h3>Aleksandra Škoro</h3>
+                    <p class="center-justified">Masažer je veoma kul. Nedavno sam ga kupila preko vašeg sajta i sada ga koristim sa velikim zadovoljstvom.
+                        Vrlo sam zadovoljna ovom kupovinom.</p>
                 </div>
             </div>
             <div>
                 <div class="rev">
                     <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/rev2.jpg" alt="">
-                    <h3>Елена Белякова</h3>
-                    <p class="center-justified">Купила этот массажер неделю назад. Пользовалась им несколько процедур, эффект стал заметен сразу. Боли в шее прошли! Очень удобно и качественно. Рекомендую.</p>
+                    <h3>Elena Maksimović</h3>
+                    <p class="center-justified">Kupila sam ovaj masažer pre nedelju dana. Koristila sam ga svega nekoliko puta i efekat već primetim.
+                        Bol u vratu je nestao! Veoma dobar i kvalitetan proizvod. Toplo preporučujem!</p>
                 </div>
             </div>
             <div>
                 <div class="rev">
                     <img src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/rev3.jpg" alt="">
-                    <h3>Анна Котова</h3>
-                    <p class="center-justified">Массажер у меня уже около месяца. Надеялась, что этот прибор избавит меня от болей на шее. И не зря! Боли прошли как будто их не бывало вообще! Всем советую!</p>
+                    <h3>Ana Kostović</h3>
+                    <p class="center-justified">Imam masažer oko mesec dana. Nadala sam se da će me ovaj uređaj osloboditi bola u leđima i to se desilo.
+                        Bolovi su nestali, kao da ih uopšte nije bilo. Savetujem svakome ko ima problema da ga kupi.</p>
                 </div>
             </div>
         </div>
@@ -142,19 +145,19 @@
 </section>
 <section class="block10">
     <div class="wrap">
-        <h2 class="title">Доставка и оплата</h2>
+        <h2 class="title">DOSTAVA I PLAĆANJE</h2>
         <ul class="ship">
             <li>
                 <div></div>
-                <p>Заполните форму для заказа товара</p>
+                <p>Unesite svoje podatke na našem sajtu kako biste poručili proizvod.</p>
             </li>
             <li>
                 <div></div>
-                <p>Оператор уточнит у вас детали и мы отправим ваш заказ</p>
+                <p>Kontaktiraćemo vas da potvrdimo vašu porudžbinu.</p>
             </li>
             <li>
                 <div></div>
-                <p>Доставка от 7 рабочих дней в зависимости от региона</p>
+                <p>Pošiljku šaljemo u roku od 1-2 radna dana. Plaćate kuriru po preuzimanju paketa.</p>
             </li>
         </ul>
     </div>
@@ -162,22 +165,22 @@
 <section class="block11">
     <div class="wrap header__wrap">
         <div class="">
-            <h2><span>Массажер </span> <span>для спины и шеи</span></h2>
+            <h2><span>MASAŽER </span> <span>ZA LEĐA I VRAT</span></h2>
             <div class="sale">
-                скидка
-                <p>-50%</p>
+                popust
+                <p>-40%</p>
             </div>
             <div class="formbox">
                 <div class="price clearfix">
                     <div class="old-cost">
-                        <span>Старая цена:</span>
+                        <span>Stara cena:</span>
                         <p>
                             {{ $prices[1]['originalPrice'] }}
                             RSD
                         </p>
                     </div>
                     <div class="new-cost">
-                        <span>Новая цена:</span>
+                        <span>Nova cena:</span>
                         <p>
                             {{ $prices[1]['amount'] }}
                             RSD
@@ -185,23 +188,25 @@
                     </div>
                 </div>
                 {{--<div class="countbox"></div>--}}
-                <form id="buy" class="forma" action="#" method="post">
-                    <input class="field" name="name" type="text" placeholder="Введите ваше имя" required>
-                    <input class="input-phone" name="phone" type="tel" placeholder="Введите ваш телефон" required>
-                    <button class="button-m" type="submit">Заказать со скидкой</button>
+                <br>
+                <form id="buy" class="forma" action="{{$orderRoute}}" method="post">
+                    {{ csrf_field() }}
+                    @include('lander.naturapharm.components.form_hidden_fields')
+                    <input class="field" name="name" type="text" placeholder="Ime i prezime" required>
+                    <input class="input-phone" name="phone" type="tel" placeholder="Telefon" required>
+                    <input class="field" name="shipping_address" type="text" placeholder="Adresa" required>
+                    <input class="field" name="shipping_city" type="text" placeholder="Grad" required>
+                    <button class="button-m" type="submit">Poručite sada</button>
                 </form>
             </div>
         </div>
-        <div class=" footer__video" style="width: 300px;">
+        <div class="footer__video" style="width: 300px;">
             <video muted="muted" width="300px" height="auto" loop="loop" preload="auto" playsinline="" webkit-playsinline="" autoplay="autoplay" controls="">
-                <source src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/1.mp4" type="video/mp4">
+                <source src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/1_1.mp4" type="video/mp4">
             </video>
         </div>
     </div>
 </section>
-<center>
-    <div id="details-wrapper"></div>
-</center>
 <script src="{{ asset('/') }}purplerelaxFiles/multifunkcionalnimasazer/main.js"></script>
 <script src="{{ asset('/') }}shared_files/slick.min.js"></script>
 @include('components.pixel_footer')
