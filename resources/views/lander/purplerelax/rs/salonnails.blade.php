540393b0a50c7be8e0a023f4819260d63e1cd909
Filip <filipnestor95@gmail.com>
2022-06-03 16:06:33 +0200

    SalonNails v1

--- resources/views/lander/purplerelax/rs/salonnails.blade.php
+++ resources/views/lander/purplerelax/rs/salonnails.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <link rel="stylesheet" type="text/css" href="{{ asset('/') }}shared_files/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/main.css" />
+    <link rel="stylesheet" href="{{ asset('/') }}purplerelaxFiles/salonnails/main.css" />
     <style>
         @font-face {
             font-family: 'Rubik';
@@ -46,18 +46,18 @@
 <div class="container">
     @include('components.display_errors')
     <header class="header">
-        <h1 class="header__title">PERFECT EYEBROW</h1>
+        <h1 class="header__title">SALON NAILS</h1>
         <a class="header__button" href="#main-form">Poruči</a>
     </header>
     <section class="offer">
         <div class="offer__main-bg">
-            <p class="offer__desc">BESPREKORNE I SAVRŠENO SREĐENE OBRVE ZA SAMO PAR SEKUNDI</p>
+            <p class="offer__desc">BESPREKORNI I SAVRŠENI NOKTI BEZ ODLASKA KOD KOZMETIČARA</p>
             <div class="offer__discount">-40%</div>
         </div>
         <ul class="offer__list">
-            <li class="offer__item">BEZBOLNO</li>
+            <li class="offer__item">PRECIZNO</li>
+            <li class="offer__item">UNIVERZALNO</li>
             <li class="offer__item">BRZO</li>
-            <li class="offer__item">PRAKTIČNO</li>
             <li class="offer__item">ELEGANTNO</li>
         </ul>
         <div class="offer__price">
@@ -79,92 +79,95 @@
     <section class="about">
         <div class="heading">
             <h2 class="basic-title about__title">
-                NAPRAVITE OBLIK OBRVA ZA SAMO 2 MINUTA
+                SREDITE SVOJE NOKTE KAO PROFESIONALAC ZA SAMO 5 MINUTA
             </h2>
         </div>
         <ul class="about__list">
-            <li class="about__item">Precizno</li>
-            <li class="about__item">Stvara savršeno glatke linije</li>
-            <li class="about__item">Uklanja mikro dlake</li>
-            <li class="about__item">Doviđenja kozmetičkim salonima</li>
+            <li class="about__item">Precizno turpija i pravi oblike</li>
+            <li class="about__item">Stvara savršeno glatke ivice</li>
+            <li class="about__item">Uklanja zanoktice</li>
+            <li class="about__item">Polira nokte</li>
         </ul>
     </section>
     <section class="choice">
         <div class="heading">
-            <h2 class="basic-title choice__title">ZVEZDE SU IZABRALE PERFECT EYEBROW</h2>
+            <h2 class="basic-title choice__title">SVE ŠTO STE DO SADA SKUPO PLAĆALI U SALONIMA, SADA MOŽETE KOD KUĆE</h2>
         </div>
         <div class="slider">
             <div class="choice__slide">
-                <img class="choice__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/choice-1.jpg" alt="" />
+                <img class="choice__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/choice-1.jpg" alt="" />
             </div>
             <div class="choice__slide">
-                <img class="choice__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/choice-2.jpg" alt="" />
+                <img class="choice__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/choice-2.jpg" alt="" />
             </div>
             <div class="choice__slide">
-                <img class="choice__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/choice-3.jpg" alt="" />
+                <img class="choice__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/choice-3.jpg" alt="" />
             </div>
         </div>
-        <p class="choice__text">Bez obzira koliko je lepa šminka, neuređene obrve pokvare celokupni dojam.</p>
-        <p class="choice__text">Sređivanje obrva je bolno i teško, a nakon toga iritacija može trajati nekoliko dana. Zaboravi na to!</p>
-        <p class="choice__text">
-            Uz pomoć <span class="flawless">PERFECT EYEBROW</span> uređaja, za nekoliko minuta dovešćete svoje obrve u savršen oblik!
-        </p>
+        <p class="choice__text">Bez obzira koliko se lepo doterate, sigurno vam se nekada dogodilo da ne stignete da sredite
+            nokte jer kod kozmetičara nije bilo slobodnih termina, ili su vam tretmani bili preskupi.</p>
+        <p class="choice__text">Kada ste pokušavali da sređujete nokte sa običnim turpijama, bilo je jako sporo i dešavalo se da ne postignete
+            ono što ste želeli.</p>
+        <p class="choice__text">Uz pomoć <span class="flawless">Salon Nails</span> uređaja, sada će vaši nokti uvek biti sređeni, a vi ćete uštedeti mnogo novca jer
+            nećete morati da odlazite kod kozmetičara.</p>
     </section>
     <section class="description">
         <div class="heading">
             <h2 class="basic-title description__title">
-                PERFECT EYEBROW - INOVATIVNI DEPILATOR ZA OBRVE
+                SALON NAILS - INOVATIVNI SET ZA SREĐIVANJE NOKTIJU
             </h2>
         </div>
         <div class="description__grid">
             <div class="description__block">
-                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/1.jpg" />
-                <div class="description__text">Kompaktan i ne zauzima mnogo prostora u torbi</div>
+                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/1.jpg" />
+                <div class="description__text">Savršeno turpija ivice noktiju</div>
             </div>
             <div class="description__block">
-                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/2.jpg" />
-                <div class="description__text">Led pozadinsko osvetljenje za savršenu preciznost</div>
+                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/2.jpg" />
+                <div class="description__text">5 različitih nastavaka</div>
             </div>
             <div class="description__block">
-                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/3.jpg" />
-                <div class="description__text">Prenosiv i na baterije</div>
+                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/3.jpg" />
+                <div class="description__text">Idealan za skidanje zanoktica</div>
             </div>
             <div class="description__block">
-                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/4.jpg" />
-                <div class="description__text">Elegantan i kvalitetan</div>
+                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/4.jpg" />
+                <div class="description__text">Led osvetljenje za potpunu preciznost</div>
             </div>
             <div class="description__block">
-                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/5.jpg" />
-                <div class="description__text">Bezbedno i bez osipa</div>
+                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/5.jpg" />
+                <div class="description__text">Polira nokte do savršenstva</div>
             </div>
             <div class="description__block">
-                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/6.jpg" />
-                <div class="description__text">Pogodno za svaku devojku</div>
+                <img class="description__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/6.jpg" />
+                <div class="description__text">Pogodan za skidanje laka</div>
             </div>
         </div>
     </section>
     <section class="prop">
         <div class="heading">
             <h2 class="basic-title prop__title">
-                UVEK LEPE I SREĐENE OBRVE
+                UVEK LEPI I SREĐENI NOKTI
             </h2>
         </div>
         <ul class="prop__list">
             <li class="prop__item">
-                <p class="prop__main">Potpuno bezbolno</p>
-                <p>Više ne morate da trpite bol koji stvara pinceta ili vosak. Iritacija i crvenilo nestaće zauvek.</p>
+                <p class="prop__main">Jeftino, brzo i pristupačno</p>
+                <p>Više ne morate da se zakazujete u skupe salone i čekate na red. Sve možete potpuno lako da obavljate
+                    kod svoje kuće.</p>
             </li>
             <li class="prop__item">
-                <p class="prop__main">Bez iritacije</p>
-                <p>Perfect Eyebrow ima 18-karatnu pozlaćenu glavu koja ne izativa alergije, crvenilo i osip kože.</p>
+                <p class="prop__main">Bez iritacije i crvenila</p>
+                <p>Salon Nails ne grebe i ne stvara iritacije pri skidanju zanoktica.</p>
             </li>
             <li class="prop__item">
-                <p class="prop__main">Za bilo koji oblik obrva</p>
-                <p>Više ne morate da idete kod kozmetičara da biste sređivali obrve - uradite to sami.</p>
+                <p class="prop__main">Za bilo koji oblik noktiju</p>
+                <p>Uz uređaj dolaze nastavci uz pomoć kojih možete da izvedete bilo koji željeni oblik noktiju.</p>
             </li>
             <li class="prop__item">
                 <p class="prop__main">Prihvaćeno od strane kozmetičara</p>
-                <p>Profesionalci u svetu ovaj uređaj sve više koriste i napuštaju zastarele tehnike kao što su pinceta, konac i vosak.</p>
+                <p>Profesionalci u svetu ovaj uređaj sve više koriste i napuštaju zastarele tehnike kao što su obične
+                    stare turpije.</p>
             </li>
         </ul>
     </section>
@@ -182,19 +185,19 @@
         </div>
         <div class="slider">
             <div class="reviews__slide">
-                <img class="reviews__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/review-1.jpg" alt="" />
+                <img class="reviews__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/review-1.jpg" alt="" />
                 <h3 class="reviews__name">Ana, 29 godina</h3>
                 <p class="reviews__text">Htela bih da izdvojim 3 razloga zbog kojih sam najzadovoljnija: sjajna korisnička služba, brza dostava (24h) i naravno vrhunski kvalitet uređaja. Sve je kao što je opisano. Hvala puno!</p>
             </div>
             <div class="reviews__slide">
-                <img class="reviews__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/review-2.jpg" alt="" />
+                <img class="reviews__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/review-2.jpg" alt="" />
                 <h3 class="reviews__name">Branka, 31 godina</h3>
-                <p class="reviews__text">Sigurno najbolje otkriće ove godine! Trošim dosta vremena i truda na sređivanje obrva, ali sada je to stvar prošlosti.
-                    Epilator uklanja dlačice veoma nežno, ne oštećuje kožu i uopste nije neprijatan, što me je iznenadilo.
+                <p class="reviews__text">Sigurno najbolje otkriće ove godine! Trošim dosta vremena i truda na sređivanje noktiju, ali sada je to stvar prošlosti.
+                    Salon Nails uređaj turpija i polira nokte veoma brzo, ne oštećuje ih i uopste nije neprijatan, što me je iznenadilo.
                     Ovo je jedan od mojih omiljenih proizvoda za negu. Naravno, veliku zahvalnost dugujem Mariji, devojci koja radi u korisničkoj podršci i koja mi je pomogla oko izbora.</p>
             </div>
             <div class="reviews__slide">
-                <img class="reviews__img" src="{{ asset('/') }}purplerelaxFiles/eyebrowremover3/review-3.jpg" alt="" />
+                <img class="reviews__img" src="{{ asset('/') }}purplerelaxFiles/salonnails/review-3.jpg" alt="" />
                 <h3 class="reviews__name">Kristina, 27 godina</h3>
                 <p class="reviews__text">Prijatno sam iznenađena svime što sam pročitala na sajtu pa sam odlučila i ja da naručim. Dugo mi je trebalo ovako nešto i kada sam videla reklamu na Facebook-u znala sam da je ovo stvar za mene.
                     Naručila sam dva komada, jedan za mene, jedan za poklon. Veoma sam zadovoljna kvalitetom izrade. Hvala PurpleRelax timu za savršen proizvod, popravili ste mi raspoloženje.</p>
@@ -222,13 +225,13 @@
     </section>
     <section class="offer offer--bottom">
         <div class="offer__main-bg">
-            <p class="offer__desc">BESPREKORNE I SAVRŠENO SREĐENE OBRVE ZA SAMO PAR SEKUNDI</p>
+            <p class="offer__desc">BESPREKORNI I SAVRŠENI NOKTI BEZ ODLASKA KOD KOZMETIČARA</p>
             <div class="offer__discount">-40%</div>
         </div>
         <ul class="offer__list">
-            <li class="offer__item">BEZBOLNO</li>
+            <li class="offer__item">PRECIZNO</li>
+            <li class="offer__item">UNIVERZALNO</li>
             <li class="offer__item">BRZO</li>
-            <li class="offer__item">PRAKTIČNO</li>
             <li class="offer__item">ELEGANTNO</li>
         </ul>
         <div class="offer__price">
