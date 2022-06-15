e61703d9dc059a176d3fa631bd6ac6860f8ffad4
Filip <filipnestor95@gmail.com>
2022-06-15 16:22:29 +0200

    Medicina Innoderm | Slimonex v3

---
+++ resources/views/lander/medicinaizprirode/rs/diabetea_adv.blade.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html dir="ltr">
+<head>
+    @include('components.pixel_init')
+    <meta charset="utf-8"/>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport"/>
+    <link href="{{ asset('/') }}natureteaFiles/diabetea_adv/combined.css" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('/') }}natureteaFiles/shared_files/logo.png" rel="icon" type="image/png">
+</head>
+<body>
+@include('components.display_errors')
+<div class="top-header">
+    <div class="container">
+        <div class="top-lft">
+            <p class="top-txt1"> Advertorijal </p>
+            <a href="#product-box"><img class="logo" src="{{ asset('/') }}natureteaFiles/shared_files/logo_invert.png"/></a>
+            <ul class="social-icon">
+                <li><a href="#product-box"><img alt="facebook" src="{{ asset('/') }}natureteaFiles/diabetea_adv/f.png"/></a></li>
+                <li><a href="#product-box"><img alt="twitter" src="{{ asset('/') }}natureteaFiles/diabetea_adv/t.png"/></a></li>
+                <li><a href="#product-box"><img alt="ln" src="{{ asset('/') }}natureteaFiles/diabetea_adv/p.png"/></a></li>
+                <li><a href="#product-box"><img alt="instagram" src="{{ asset('/') }}natureteaFiles/diabetea_adv/l.png"/></a></li>
+                <li><a href="#product-box"><img alt="instagram" src="{{ asset('/') }}natureteaFiles/diabetea_adv/i.png"/></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="menu">
+    <div class="container">
+        <ul class="deskmenu">
+            <li><a href="#product-box"><img src="{{ asset('/') }}natureteaFiles/diabetea_adv/home.png"/> </a></li>
+            <li><a class="active" href="#product-box">  Zdravlje  </a></li>
+            <li><a href="#product-box">  Fitnes
+                </a>
+            </li>
+            <li><a href="#product-box">  Hrana  </a>
+            </li>
+            <li><a href="#product-box">  Lepota  </a>
+            </li>
+            <li><a href="#product-box">  Koža  </a>
+            </li>
+            <li><a href="#product-box">  Stil života  </a>
+            </li>
+            <li><a href="#product-box">  Sport  </a>
+            </li>
+            <li><a href="#product-box">  Putovanja  </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="mobile-hdr">
+    <div class="navwrp">
+        <a class="smobitrigger mob-menu mobmenu-click inrwrpr" href="#product-box">
+            <img src="{{ asset('/') }}natureteaFiles/diabetea_adv/mob-menu-btn.png"/></a>
+        <ul class="mobimenu inrwrpr">
+            <li><a href="#product-box">  Glavna  </a>
+            </li>
+            <li><a href="#product-box">  Zdravlje  </a>
+            </li>
+            <li><a class="active" href="#product-box">  Fitnes   </a></li>
+            <li><a href="#product-box">  Hrana  </a>
+            </li>
+            <li><a href="#product-box">  Lepota  </a>
+            </li>
+            <li><a href="#product-box">  Koža  </a>
+            </li>
+            <li><a href="#product-box">  Stil života  </a>
+            </li>
+            <li><a href="#product-box">  Sport  </a>
+            </li>
+            <li><a href="#product-box">  Putovanja  </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="section1">
+    <div class="container">
+        <p class="s1-list">
+            <span class="s1-sp1">  Glavna  </span>
+            <span class="s1-sp2">  Zdravlje  </span>
+            <span class="s1-sp3">  Šećer u krvi   </span>
+        </p>
+        <h2 class="sec1-p2"><span>  4 tajne  </span> za snižavanje nivoa šećera u krvi </h2>
+        <div class="left-sec">
+            <div class="div-post">
+                <div class="face-box">
+                    <img alt="face" class="face" src="{{ asset('/') }}natureteaFiles/diabetea_adv/face.png"/>
+                    <p> Piše <b>   Milan Mitrović  </b> <span class="dt-spn">   |   <br class="show-mob"/>
+                        </span>
+                    </p>
+                </div>
+                {{--<img alt="face" class="so-ics" src="{{ asset('/') }}natureteaFiles/diabetea_adv/so-ics.png"/>--}}
+            </div>
+            <img alt="img" class="seen" src="{{ asset('/') }}natureteaFiles/diabetea_adv/seen.png"/>
+            <img alt="img" class="img-1" src="{{ asset('/') }}natureteaFiles/diabetea_adv/img-1.jpg"/>
+            <p class="comn-para"> Nivo šećera u krvi je glavna briga ljudi sa dijabetesom. Visok šećer u krvi, poznat kao hiperglikemija, pojavljuje se kada je kod osobe nivo šećera u krvi preko 180 miligrama po decilitru (mg/dL). </p>
+            <p class="comn-para"> A da li ste znali da poremećen nivo šećera u krvi može da ima negativne posledice na zdravlje i kvalitet života za "preddijabetičare" koji osećaju simptome, a da to i ne znaju?
+            </p>
+            <p class="comn-para"> U ovom članku pregledamo top 4 tajne za snižavanje šećera u krvi.
+            </p>
+            <p class="txt-head"> Zašto je kontrola šećera u krvi važna? </p>
+            <img alt="img" class="img-1" src="{{ asset('/') }}natureteaFiles/diabetea_adv/img-2.jpg"/>
+            <p class="comn-para"> Održavanje šećera u krvi u ciljanim vrednostima može pomoći da izbegnete zdravstvene probleme sa visokim šećerom u krvi. </p>
+            <p class="comn-para"> Vremenom, nezdravi nivo šećera u krvi može da ošteti sitne i velike krvne sudove u nekoliko organa i sistema što dovodi do ozbiljnih posledica koje uključuju: <span class="red-clr"><b>  oštećenje vida i slepoću, čireve na stopalima, infekcije i amputacije, otkazivanje bubrega i dijalizu, srčani i moždani udar, bolesti perifernog vaskularnog sistema i štetu na nervnom sistemu što dovodi do bola i slabosti.  </b></span></p>
+            <p class="comn-para"><a href="#product-box"><span class="blue-clr"><u><b>  Pogledajte ovu zapanjujuću statistiku: do 90% ljudi u Srbiji ima poremećen nivo šećera u krvi.</b></u></span></a> Osećate li glad više nego što je to normalno? Ili ste žedni više nego obično? Osećate li bolove u gornjem delu nogu ili osećate da su vam gležnjevi i stopala natečeni? Ako je tako, onda postoji velika šansa da patite od posledica povećanog nivoa šećera u krvi.
+            </p>
+            <p class="comn-para"> Kada održavate nivo šećera u krvi ispod 100 mg/dL pre jela i ispod 180 mg/dL nakon jela, možete značajno da snizite rizik od neželjenih efekata od poremećenog nivoa šećera u krvi. </p>
+            <p class="txt-head"> Kako sniziti nivo šećera u krvi </p>
+            <img alt="img" class="img-1" src="{{ asset('/') }}natureteaFiles/diabetea_adv/img-3.jpg"/>
+            <p class="comn-para"> Evo 4 top načina na koja možete sniziti visok nivo šećera u krvi i sniziti rizik od komplikacija: </p>
+            <p class="head-p2"><span>1</span> Smanjite unos ugljenih hidrata </p>
+            <p class="comn-para"> Istraživači su sproveli studije koje pokazuju da uzimanje hrane sa niskim udelom ugljenih hidrata, a visokim sadržajem proteina, smanjuje nivo šećera u krvi. </p>
+            <p class="comn-para"> Telo razlaže ugljene hidrate u šećere koje telo koristi kao energiju. Neki ugljeni hidrati su neophodni u ishrani. Međutim, za mnoge ljude (posebno one sa dijabetesom) uzimanje previše ugljenih hidrata može da prouzrokuje preterani skok šećera u krvi. </p>
+            <p class="comn-para"> Smanjenje količine ugljenih hidrata koje čovjek unosi smanjuje skokove šećera u krvi. </p>
+            <p class="head-p2"><span>2</span> Jedite ispravne ugljene hidrate </p>
+            <p class="comn-para"> Dve glavne vrste ugljenih hidrata su prosti i složeni i različito utiču na nivo šećera u krvi. </p>
+            <p class="comn-para"> Prosti ugljeni hidrati su uglavnom sačinjeni od jedne vrste šećera. Nalaze se u hrani poput belog hleba, testenine i slatkiša. Telo razlaže ove ugljene hidrate u šećere veoma brzo što uzrokuje da se nivo šećera u krvi brzo digne. </p>
+            <p class="comn-para"> Složeni ugljeni hidrati sačinjeni su od tri ili više vrsta šećera koji su povezani zajedno. Zato što su hemijske veze među ovim vrstama šećera složene, telu treba više vremena za razlaganje. </p>
+            <p class="comn-para"> Kao rezultat, šećer se otpušta u telo postepeno, što znači da nivo šećera u krvi ne raste brzo nakon jela. Primeri složenih ugljenih hidrata uključuju celovite žitarice i slatki krompir. </p>
+            <p class="head-p2"><span>3</span> Izaberite hranu sa niskim glikemijskim indeksom </p>
+            <p class="comn-para"> Glikemijski indeks meri i rangira raznu hranu prema tome kako uzrokuju podizanje nivoa šećera u krvi. Istraživanja pokazuju da poštovanje ishrane sa niskim glikemijskim indeksom smanjuje brzi skok nivoa šećera u krvi.
+            </p>
+            <p class="comn-para"> Hrana sa niskim glikemijskim indeksom je ona koja ima vrednost nižu od 55. Primeri hrane sa niskim glikemijskim indeksom su: slatki krompir, kinoa, mahunarke, mleko sa niskim sadržajem masti, zeleno lisnato povrće, neskrobno povrće, orašasti plodovi i semenke, meso i riba. </p>
+            <p class="head-p2"><span>4</span> Redovno uzimanje suplemenata za kontrolu šećera u krvi </p>
+            <p class="comn-para"> Suplementi za održavanje nivoa šećera u krvi kao što je <a href="#product-box"><span class="blue-clr"><b>  DiabeTea</b></span></a> često se pominju u medijima u poslednje vreme i to sa dobrim razlogom. </p>
+            <p class="comn-para"> Sačinjen od prirodnih, klinički testiranih sastojaka <a href="#product-box"><span class="blue-clr"><b>  DiabeTea</b></span></a> dokazano je efikasan u stabilizovanju nivoa šećera u krvi. Glavne prednosti uzimanja suplemenata uključuju podršku zdravog funkcionisanja metabolizma, podsticanje zdravog metabolizma ugljenih hidrata i lipida, održavanje zdravog nivoa šećera u krvi. </p>
+            <p class="comn-para"> Dodatni bonus je to što pomaže kod mršavljenja jer visok nivo šećera u krvi pokreće ispuštanje viška insulina što sprečava telo u sagorevanju masti. </p>
+            <p class="comn-para"> Najbolje od svega je da se lako uzima. Skuvajte i pijte nezaslađen čaj nekoliko puta dnevno. Zato što je napravljen od prirodnih sastojaka nije potreban recept za kupovinu. </p>
+            <p class="txt-head"> Šta još treba da znam? </p>
+            <p class="comn-para"> Kratka verzija? Ako patite od štetnih efekata povećanog šećera u krvi, treba da kupite <a href="#product-box"><span class="blue-clr"><b>  DiabeTea</b></span></a> odmah. </p>
+            <p class="comn-para"> Potrebno vam je još informacija? Evo još podataka o tome zbog čega je <a href="#product-box"><span class="blue-clr"><b>  DiabeTea</b></span></a> tako odličan: </p>
+            <p class="comn-para bg-tik"><b>  STVARNO je prirodan.   </b><br/> <a href="#product-box"><span class="blue-clr bld"><b>  DiabeTea</b></span></a> je potpuno prirodno rešenje koje uključuje mešavinu biljnih ekstrakata. Recept nije potreban. </p>
+            <p class="comn-para bg-tik"><b>  Garantovano pomaže.   </b><br/> <a href="#product-box"><span class="blue-clr bld"><b>  DiabeTea</b></span></a> ima <a href="#product-box"><span class="blue-clr bld"><b><u>  100% garanciju zadovoljstva</u></b></span></a> što znači da nemate šta da izgubite. </p>
+            <p class="txt-head"> Gde da ga kupim? </p>
+            <p class="comn-para"> Ne padajte na jeftine kopije na internetu. Posetite njihovu stranicu da kupite pravi <a href="#product-box"><span class="blue-clr bld"><b>  DiabeTea  </b></span></a> odmah. </p>
+            <p class="txt-head f-26">Prestanite da bacate novac. Preuzmite kontrolu već danas.</p>
+            <p class="comn-para"> Ako vam povišeni nivo šećera u krvi uzrokuje frustraciju i smanjuje vam kvalitet života, onda vam je potrebno bolje rešenje. Preuzmite kontrolu i uzmite <a href="#product-box"><span class="blue-clr bld"><b>  DiabeTea.  </b></span></a></p>
+            <p class="special-p"> Posebna akcija za naše čitaoce </p>
+            <img alt="img" class="secr" src="{{ asset('/') }}natureteaFiles/diabetea_adv/secr.png"/>
+            <div class="product-box" id="product-box">
+                <div class="prd-lft">
+                    <img alt="img" class="btls" src="{{ asset('/') }}natureteaFiles/diabetea/prod.png"/>
+                </div>
+                <div class="prd-rit">
+                    <p class="prd-p3">Unesite podatke za dostavu</p>
+                    <form action="{{$orderRoute}}" method="POST" id="order-form">
+                        {{ csrf_field() }}
+                        @include('lander.naturapharm.components.form_hidden_fields')
+                        <input type="text" name="name" class="form-input" placeholder="Ime i prezime" required/>
+                        <input type="text" name="phone" class="form-input" placeholder="Broj telefona" required/>
+                        <input type="text" name="shipping_address" class="form-input" placeholder="Adresa" required/>
+                        <input type="text" name="shipping_city" class="form-input" placeholder="Grad" required/>
+                        <select class="form-input change-package-selector" id="product" name="quantity" required>
+                            @foreach($prices as $singlePrice)
+                                <option value="{{ $singlePrice['quantity'] }}" placeholder="{{ $singlePrice['is_free_shipping'] }}"
+                                        @if($singlePrice['is_default']) selected @endif
+                                >{{ $singlePrice['quantity'] }} x {{ $product->product_name }} ({{ $singlePrice['amount'] }} RSD)
+                                    @if($singlePrice['is_free_shipping']) * @endif
+                                </option>
+                            @endforeach
+                        </select>
+                        <div class="freeShippingDiv">* BESPLATNA DOSTAVA</div>
+                        <button type="submit" class="btn">PORUČI ODMAH</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        {{--<div class="rit-sec">--}}
+            {{--<div class="clearall"></div>--}}
+        {{--</div>--}}
+        <div class="left-sec">
+            <p class="cmnt-p1"> Komentari <span><img alt="img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/msg.png"/> <img alt="img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/print.png"/></span></p>
+            <p class="cmnt-p2"></p>
+            <div class="comnt-div">
+                <img alt="img" class="fac1-img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/fac1.png"/>
+                <p class="cmnt-p3"> Sandra <img alt="img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/star.png"/> <span>   Top komentari  </span></p>
+                <p class="cmnt-p4"> Sviđa mi se osećaj u nogama! Bilo mi je baš teško dugo vremena. Zaboravila sam kako je kad su noge zdrave. </p>
+                <p class="cmnt-p5"></p>
+            </div>
+            <div class="comnt-div">
+                <img alt="img" class="fac1-img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/fac2.png"/>
+                <p class="cmnt-p3"> Bojan • <img alt="img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/follow.png"/> <span>
+                     172 pratioca  </span>
+                </p>
+                <p class="cmnt-p4"> Već mnogo meseci se borim protiv visokog šećera u krvi i znao sam da sam na putu da uzimam lekove za normalizaciju. I mnogo me je obradovalo kada sam uspeo sam da ga regulišem. </p>
+                <p class="cmnt-p5"></p>
+            </div>
+            <div class="comnt-div">
+                <img alt="img" class="fac1-img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/fac3.png"/>
+                <p class="cmnt-p3"> Jasmina • <img alt="img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/follow.png"/> <span>
+                     122 pratilaca  </span>
+                </p>
+                <p class="cmnt-p4"> Mogu da hodam po kući i dvorištu sa lakoćom što nisam mogla pre 2 meseca zato što sam bila skoro vezana za krevet zbog otrnulosti nogu </p>
+                <p class="cmnt-p5"></p>
+            </div>
+            <div class="comnt-div">
+                <img alt="img" class="fac1-img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/fac4.png"/>
+                <p class="cmnt-p3"> Milica • <img alt="img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/follow.png"/>
+                    <span>  67 pratilaca  </span>
+                </p>
+                <p class="cmnt-p4"> Toplo mi je i sviđa mi se! Zbog loše cirkulacije su mi noge i ruke stalno bile hladne po ceo dan. A sada toga više nema. </p>
+                <p class="cmnt-p5"></p>
+            </div>
+            <div class="comnt-div">
+                <img alt="img" class="fac1-img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/fac5.png"/>
+                <p class="cmnt-p3"> Radovan • <img alt="img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/follow.png"/>
+                    <span>   49 pratilaca  </span>
+                </p>
+                <p class="cmnt-p4"> Igrao sam po prvi put za 10 godina. Nikad nisam mislio da će mi noge biti dovoljno zdrave da mogu ponovo da zaigram. Srećom, nisam bio u pravu. </p>
+                <p class="cmnt-p5"></p>
+            </div>
+            <div class="comnt-div no-brd">
+                <img alt="img" class="fac1-img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/fac6.png"/>
+                <p class="cmnt-p3"> Ana • <img alt="img" src="{{ asset('/') }}natureteaFiles/diabetea_adv/follow.png"/> <span>
+                     38 pratilaca  </span>
+                </p>
+                <p class="cmnt-p4"> Ponovo se osećam normalno. Noge mi izgledaju normalno i osećam se puno bolje nego ranije. </p>
+                <p class="cmnt-p5"></p>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="container">
+        <img alt="img" class="footer-logo" src="{{ asset('/') }}natureteaFiles/shared_files/logo_invert.png"/>
+        <p class="f-p1"> © DiebeTea
+            <span class="ryear"></span>
+        </p>
+        <div class="clearall"></div>
+        <ul class="foo-ul">
+            <li><a href="#product-box"><img src="{{ asset('/') }}natureteaFiles/diabetea_adv/ff.png"/></a></li>
+            <li><a href="#product-box"><img src="{{ asset('/') }}natureteaFiles/diabetea_adv/ft.png"/></a></li>
+            <li><a href="#product-box"><img src="{{ asset('/') }}natureteaFiles/diabetea_adv/fp.png"/></a></li>
+            <li><a href="#product-box"><img src="{{ asset('/') }}natureteaFiles/diabetea_adv/fl.png"/></a></li>
+            <li><a href="#product-box"><img src="{{ asset('/') }}natureteaFiles/diabetea_adv/fi.png"/></a></li>
+        </ul>
+    </div>
+</footer>
+<style>
+    .freeShippingDiv {
+        color: #ff0000;
+        text-align: center;
+        display: none;
+        font-weight: bold;
+        margin-top: 20px;
+    }
+</style>
+<script>
+    $('.change-package-selector').click(function () {
+        let fsh = $('option:selected', this).attr('placeholder');
+        if(fsh == 1) {
+            $(this).next('.freeShippingDiv').slideDown();
+        } else {
+            $(this).next('.freeShippingDiv').slideUp();
+        }
+    })
+</script>
+@include('components.pixel_footer')
+</body>
+</html>
