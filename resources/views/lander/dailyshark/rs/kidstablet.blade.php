fddb4b29d468f9b67caf79780fa0f3495a37f2af
Filip <filipnestor95@gmail.com>
2022-10-21 16:35:59 +0200

    KidsTablet v1

--- resources/views/lander/dailyshark/rs/kidstablet.blade.php
+++ resources/views/lander/dailyshark/rs/kidstablet.blade.php
@@ -44,19 +44,20 @@
 </head>
 <body>
 <div class="main_wrapper">
+    @include('components.display_errors')
     <section class="offer">
-        <h1 class="main_title" style="color:#013162;">Графический планшет для творчества</h1>
+        <h1 class="main_title" style="color:#013162;">TABLET ZA RAZVIJANJE KREATIVNOSTI KOD DECE</h1>
         <div class="box">
             <img src="{{ asset('/') }}dailysharkFiles/kidstablet/bgg.jpg" alt="">
             <div class="sale">
-                <p>скидка <span>50%</span></p>
+                <p>popust <span>40%</span></p>
             </div>
         </div>
         <div class="bottom">
             <ul class="list1">
-                <li>Вы можете создавать рисунки</li>
-                <li>Чертить все, что вам нужно</li>
-                <li>Писать или делать подсчеты</li>
+                <li>Crtanje i pisanje</li>
+                <li>Učenje slova i brojeva</li>
+                <li>Deca više neće pistati po zidovima</li>
             </ul>
             <div class="price clearfix">
                 <div class="old">
@@ -76,136 +77,137 @@
                     </small>
                 </div>
             </div>
-            <a href="#order_form" class="button-m">Заказать со скидкой</a>
-            <p class="products_count">Осталось <span>18</span> планшетов по акции</p>
+            <a href="#order_form" class="button-m">Poručite sada</a>
+            <p class="products_count">Preostalo <span>18</span> komada na akciji</p>
         </div>
     </section>
     <section class="sect2 spisok3">
-        <h2 class="title">  <span>Особенности планшета</span>
-        </h2>
+        <h2 class="title"><span>KARAKTERISTIKE TABLETA</span></h2>
         <ul>
             <li>
                 <img src="{{ asset('/') }}dailysharkFiles/kidstablet/s2_img3.jpg">
-                <h3>Удаление записей одной кнопкой</h3>
+                <h3>BRISANJE ZAPISA JEDNIM DUGMETOM</h3>
             </li>
             <li>
                 <img src="{{ asset('/') }}dailysharkFiles/kidstablet/s2_img2.jpg">
-                <h3>Стилус крепится прямо на корпус</h3>
+                <h3>OLOVKA JE PRIČVRŠĆENA DIREKTNO NA KUĆIŠTE</h3>
             </li>
             <li>
                 <img src="{{ asset('/') }}dailysharkFiles/kidstablet/s2_img1.jpg">
-                <h3>Безвреден для глаз</h3>
+                <h3>NE ŠTETI I NE NAPREŽE OČI</h3>
             </li>
             <li>
                 <img src="{{ asset('/') }}dailysharkFiles/kidstablet/s2_img4.jpg">
-                <h3>Очень легкий и тонкий</h3>
+                <h3>VEOMA JE LAGAN I TANAK</h3>
             </li>
         </ul>
-        <a href="#order_form" class="button-m">Заказать со скидкой</a>
+        <a href="#order_form" class="button-m">Poručite sada</a>
     </section>
     <section class="sect3 color_theme">
-        <h2 class="title">Лучшая игрушка для маленьких непосед</h2>
+        <h2 class="title">NAJBOLJA IGRAČKA ZA SMIRIVANJE MALIŠANA</h2>
         <img src="{{ asset('/') }}dailysharkFiles/kidstablet/s3_img.jpg" alt="">
-        <p>Планшет для рисования Writing Tablet Planshet это источник нескончаемого вдохновения. Ребенок может рисовать что-то новое
-            снова и снова, при этом вам не придется тратить деньги на покупку новых альбомов или бумаги.
+        <p>
+            Tablet za decu je izvor beskrajne inspiracije. Dete može da crta nešto novo iznova i iznova, a vi ne morate
+            da trošite novac na kupovinu novih bojanki i papira. Takođe, deca neće šarati po vašim zidovima.
         </p>
         <ul class="list1">
-            <li>Улучшает творческие способности</li>
-            <li>Повышает умственную активность</li>
-            <li>Развивает моторику</li>
-            <li>Формирует ребенка как личность</li>
+            <li>Poboljšava kreativnost</li>
+            <li>Povećava mentalnu aktivnost</li>
+            <li>Razvija motoričke sposobnosti</li>
+            <li>Potpuno bezbedna i zdrava zanimacija za dete</li>
         </ul>
         <br>
     </section>
     <section class="sect4">
         <img src="{{ asset('/') }}dailysharkFiles/kidstablet/colors.jpg" alt="">
-        <h4>Характеристики:</h4>
+        <h4>KARAKTERISTIKE:</h4>
         <ul class="comlect">
             <li>
                 <span>1</span>
-                <p>Материал: монохромный LCD с пленкой, материал корпуса: ABS пластик</p>
+                <p>Materijal: Monohromatski LCD sa filmom <br>Materijal kućišta: ABS plastika</p>
             </li>
             <li>
                 <span>2</span>
-                <p>Комплектация: планшет, перо</p>
+                <p>Sadržaj pakovanja: tablet, olovka</p>
             </li>
             <li>
                 <span>3</span>
-                <p>Диагональ экрана: 12 дюймов</p>
+                <p>Dijgonala ekrana: 12 inča</p>
             </li>
             <li>
                 <span>4</span>
-                <p>Вес: 260 грамм</p>
+                <p>Težina: 260 grama</p>
             </li>
             <li>
                 <span>5</span>
-                <p>Размер в упаковке: 280.8 х 185 х 4.8 мм</p>
+                <p>Veličina pakovanja: 280,8 x 185 x 4,8 mm</p>
             </li>
         </ul>
-        <a href="#order_form" class="button-m">Заказать со скидкой</a>
+        <a href="#order_form" class="button-m">Poručite sada</a>
     </section>
     <section class="rev1 color_theme">
-        <h2 class="title">Отзывы покупателей</h2>
+        <h2 class="title">UTISCI KUPACA</h2>
         <div class="reviews_slider_rev1">
             <div class="rev1-inner">
                 <img src="{{ asset('/') }}dailysharkFiles/kidstablet/rev3.jpg" alt="">
-                <p>
-                    Мои дети очень любят рисовать. Помимо рисования на бумаге, мы ещё иногда рисуем на мольберте. Поэтому, когда я увидела современный LCD планшет, то сразу же решила его купить. Цена составила всего
-                    {{ $prices[1]['amount'] }}
-                    {{ $prices[1]['currency'] }}
-                    . Кстати, подобные планшеты я ещё видела в магазине Светофор, но в разы дороже.
-                </p>
-                <b class="info">Ольга, 28 лет</b>
+                <p>Moja deca vole da crtaju. Pored crtanja na papiru, ponekad crtamo i na štafelaju. Video sam moderan tablet
+                    i odmah sam odlučio da ga kupim. Cena je bila vrlo prihvatljiva. Uzgred, video sam slične tablete u dečijim
+                    prodavnicama ali su bili mnogo skuplji.</p>
+                <b class="info">Ivan, 28 godina</b>
             </div>
             <div class="rev1-inner">
                 <img src="{{ asset('/') }}dailysharkFiles/kidstablet/rev2.jpg" alt="">
-                <p>У дочки в классе стали появляться планшеты для рисования Writing Tablet Planshet и она захотела себе такой. Поэтому и заказали у вас на сайте. Хотя подобные игрушки у нее были и ранее, но данный планшет сделан как настоящий: стильный и тонкий, а также качество очень хорошое. </p>
-                <b class="info">Анастасия, 34 года</b>
+                <p>Moja kćerka je počela da kupuje bojanke ali ih je brzo prevazišla i postale su joj dosadne. Kupila sam jojž
+                    ovaj tablet i moram da vam priznam da ju je to fasciniralo. Potpuno je oduševljena poklonom.
+                    Preporučujem ga svima koji imaju decu, neverovatna zabava za jako malo novca.</p>
+                <b class="info">Anastasija, 34 godine</b>
             </div>
             <div class="rev1-inner">
                 <img src="{{ asset('/') }}dailysharkFiles/kidstablet/rev1.jpg" alt="">
-                <p>Тоже купили детям этот Writing Tablet Planshet. Этот планшет удобно брать в дорогу, мало места занимает и писать на нем можно очень долго (заявлено, что выдержит более 500 000 рисунков), а если взять предмет похожий на стилус, то можно и вдвоем рисовать/играть (в крестики-нолики, к примеру). </p>
-                <b class="info">Олег, 31 год</b>
+                <p>Kupili smo ovaj tablet za pisanje deci. Veoma je pogodan za poneti na put i deca se uglavnom mirna dok se voze.
+                    Što je najvažnije, ne boje mi više sedišta u automobilu, to me je ranije izluđivalo. Super stvar, neka svaki
+                    roditelj kupi svome detetu.</p>
+                <b class="info">Jovan, 31 godina</b>
             </div>
         </div>
-        <a href="#order_form" class="button-m">Заказать со скидкой</a>
+        <a href="#order_form" class="button-m">Poručite sada</a>
     </section>
     <section class="vote color_theme">
-        <h2 class="title">Понравилась ли вашему ребенку игрушка?</h2>
+        <h2 class="title">DA LI SE VAŠEM DETETU DOPAO TABLET?</h2>
         <div class="addreview">
             <div class="width">
                 <div class="coll-50 reviews_test">
                     <div class="test">
                         <div class="review_result">
-                            <p>Да, ребенок просто в восторге!</p>
+                            <p>Da, dete je oduševljeno</p>
                             <div class="widthpoll">
                                 <div class="line green_result" style="width: 76%;"></div>
                                 <div class="result_item">76%</div>
                             </div>
                         </div>
                         <div class="review_radio green_radio">
-                            <input type="radio"> <span>Да, ребенок просто в восторге!</span>
+                            <input type="radio"> <span>Da, dete je oduševljeno</span>
                         </div>
                         <div class="review_result">
-                            <p>Нет, не то, что я ожидал(а)</p>
+                            <p>Ne, nije ono što sam očekivao</p>
                             <div class="widthpoll">
                                 <div class="line red_result" style="width: 2%;"></div>
                                 <div class="result_item">2%</div>
                             </div>
                         </div>
                         <div class="review_radio red_radio">
-                            <input type="radio"> <span>Нет, не то, что я ожидал(а)</span>
+                            <input type="radio"> <span>Ne, nije ono što sam očekivao</span>
                         </div>
                         <div class="review_result">
-                            <p>Не покупал(а), узнать результаты</p>
+                            <p>Nisam još kupio ali bih pogledao rezultat</p>
                             <div class="widthpoll">
                                 <div class="line blue_result" style="width: 22%;"></div>
                                 <div class="result_item">22%</div>
                             </div>
-                            <p class="votenum">Проголосовало пользователей: <span>874</span></p>
+                            <p class="votenum">Broj glasova: <span>874</span></p>
                         </div>
                         <div class="review_radio blue_radio">
-                            <input type="radio"> <span>Не покупал(а), узнать результаты</span>
+                            <input type="radio"> <span>Nisam još kupio ali bih pogledao rezultat</span>
                         </div>
                     </div>
                 </div>
@@ -213,24 +215,23 @@
         </div>
     </section>
     <section class="deliv1">
-        <h2 class="title">Как мы работаем</h2>
+        <h2 class="title">KAKO RADIMO</h2>
         <div class="deliv1_inner">
             <div class="blue1 di1">
-                <h3>Доставка</h3>
-                <p>Доставка почтой в течение 3-10 рабочих дней.
-                </p>
+                <h3>ISPORUKA</h3>
+                <p>Dostava poštom u roku od 3-10 radnih dana</p>
             </div>
         </div>
         <div class="deliv1_inner">
             <div class="purple di2">
-                <h3>Гарантии</h3>
-                <p>Мы всегда проверяем товар перед отправкой и гарантируем 100% качество.</p>
+                <h3>GARANCIJA</h3>
+                <p>Uvek proveravamo robu pre isporuke i garantujemo 100% kvalitet</p>
             </div>
         </div>
         <div class="deliv1_inner">
             <div class="green di3">
-                <h3>ОПЛАТА</h3>
-                <p>Оплата заказов осуществляется непосредственно по факту получения заказа.</p>
+                <h3>PLAĆANJE</h3>
+                <p>Plaćanje se vrši kuriru pri preuzimanju pošiljke</p>
             </div>
         </div>
         <div class="deliv1_inner">
@@ -238,18 +239,18 @@
         </div>
     </section>
     <section class="offer">
-        <h1 class="main_title" style="color:#013162;">Графический планшет для творчества</h1>
+        <h1 class="main_title" style="color:#013162;">TABLET ZA RAZVIJANJE KREATIVNOSTI KOD DECE</h1>
         <div class="box">
             <img src="{{ asset('/') }}dailysharkFiles/kidstablet/bgg.jpg" alt="">
             <div class="sale">
-                <p>скидка <span>50%</span></p>
+                <p>popust <span>40%</span></p>
             </div>
         </div>
         <div class="bottom">
             <ul class="list1">
-                <li>Вы можете создавать рисунки</li>
-                <li>Чертить все, что вам нужно</li>
-                <li>Писать или делать подсчеты</li>
+                <li>Crtanje i pisanje</li>
+                <li>Učenje slova i brojeva</li>
+                <li>Deca više neće pistati po zidovima</li>
             </ul>
             <div class="price clearfix">
                 <div class="old">
@@ -269,20 +270,23 @@
                     </small>
                 </div>
             </div>
-            <h4>Оставьте заявку</h4>
-            <p>и получите планшет для рисования Writing Tablet Planshet <span>по скидке 50%</span>
-            </p>
-            <form id="order_form" class="main-order-form m1-form"
-                  action="#" method="post">
-                <input name="name" type="text" placeholder="Введите ваше имя" required="">
-                <input name="phone" type="tel" placeholder="Введите ваш телефон" required="">
-                <button class="button-m">Заказать со скидкой</button>
+            <h4>UNESITE PODATKE ZA DOSTAVU</h4>
+            <p>i nabavite tablet za decu <span>sa 40% popusta</span></p>
+            <form id="order_form" class="main-order-form m1-form" action="{{$orderRoute}}" method="post">
+                {{csrf_field()}}
+                @include('lander.naturapharm.components.form_hidden_fields')
+                <input name="name" type="text" placeholder="Ime i prezime" required="">
+                <input name="phone" type="tel" placeholder="Telefon" required="">
+                <input name="name" type="text" placeholder="Adresa" required="">
+                <input name="name" type="text" placeholder="Grad" required="">
+                <button class="button-m">Poručite sada</button>
             </form>
-            <p class="products_count">Осталось <span>18</span> планшетов по акции</p>
+            <p class="products_count">Preostalo <span>18</span> komada na akciji</p>
         </div>
     </section>
 </div>
 <script src="{{ asset('/') }}shared_files/slick.min.js"></script>
 <script src="{{ asset('/') }}dailysharkFiles/kidstablet/init.js"></script>
+@include('components.pixel_footer')
 </body>
 </html>
