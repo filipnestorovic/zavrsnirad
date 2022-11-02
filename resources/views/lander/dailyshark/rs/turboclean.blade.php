511e1030b5f5411ab770520b707164a4460b890e
Filip <filipnestor95@gmail.com>
2022-11-02 12:07:06 +0100

    TurboClean v2

--- resources/views/lander/dailyshark/rs/turboclean.blade.php
+++ resources/views/lander/dailyshark/rs/turboclean.blade.php
@@ -43,27 +43,28 @@
     </style>
 </head>
 <body>
+@include('components.display_errors')
 <header class="offer_section">
     <div class="wrapper scroll">
-        <h4 class="subtitle">Чистящая щетка</h4>
-        <h1 class="main_title">Turbo Scrub 360</h1>
-        <p class="description">тяжелая уборка одним нажатием кнопки</p>
-        <div class="discount">Акция! <span>50%</span> скидка</div>
+        <h4 class="subtitle">ČETKA ZA ČIŠĆENJE</h4>
+        <h1 class="main_title">TURBO CLEAN</h1>
+        <p class="description">ČIŠĆENJE PRITISKOM NA DUGME</p>
+        <div class="discount">Akcija! <span>40%</span> popusta</div>
         <ul>
-            <li>Удобный, быстрый и легкий</li>
-            <li>Хорошая производительность очистки</li>
-            <li>360 оборотов в минуту</li>
+            <li>Pogodno, brzo i lagano</li>
+            <li>Dobre performanse čišćenja</li>
+            <li>360 okretaja u minuti</li>
         </ul>
         <div class="price_block clearfix">
             <div class="price_item old">
-                <div class="text">Обычная цена:</div>
+                <div class="text">Redovna cena:</div>
                 <div class="value">
                     {{ $prices[1]['originalPrice'] }}
                     {{ $prices[1]['currency'] }}
                 </div>
             </div>
             <div class="price_item new">
-                <div class="text">Цена сегодня:</div>
+                <div class="text">Cena sada:</div>
                 <div class="value">
                     {{ $prices[1]['amount'] }}
                     {{ $prices[1]['currency'] }}
@@ -75,62 +76,65 @@
 </header>
 <section class="about_section">
     <div class="wrapper clearfix">
-        <h2>Чистящая щетка <span>Turbo Scrub 360</span></h2>
-        <img src="{{ asset('/') }}dailysharkFiles/turboclean/about__image.jpg" alt="Чистящая щетка Turbo Scrub 360">
+        <h2><span>TURBO CLEAN</span></h2>
+        <img src="{{ asset('/') }}dailysharkFiles/turboclean/about__image.jpg" alt="Turbo Clean">
         <div class="text_block">
-            <p style="font-size: 20px;">Теперь вы можете убрать весь арсенал своей прачечной и сделать тяжелую уборку одним нажатием кнопки! Познакомьтесь с Turbo Scrub 360! Мощное автоматическое решение, которое освобождает вас от стресса! За несколько минут Turbo Scrub 360 удаляет грязь и ил из плиток, устраняет жир, накопленный в раковине и кухонной плите, и даже моет лобовое стекло вашего автомобиля гораздо более практичным и быстрым способом! Его долговечная батарея заставляет голову вращаться триста шестьдесят раз в минуту, чтобы сделать быстрый скраб, который превышает качество ручной очистки! Turbo Scrub 360 очень прост в использовании, очень быстрый и интуитивно понятный для сборки и немедленного использования.</p>
+            <p style="font-size: 20px;">
+                Sada možete da perete tepihe, staze i obavite sva teška čišćenja jednim pritiskom na dugme! <br><br>
+                Upoznajte <b>TURBO CLEAN!</b> Moćno rešenje koje vas oslobađa od stresa i napornog ribanja. Za samo nekoliko minuta TURBO CLEAN uklanja
+                prljavštinu i kamenac sa pločica, eliminiše masnoću nakupljenu u lavabou i šporetu, pa čak čisti i šoferšajbnu automobila.<br><br>
+                Dugotrajna baterija čini da se glava okreće 360 puta u minuti kako bi se napravilo brzo ribanje koje prevazilazi kvalitet ručnog ribanja!
+                TURBO CLEAN je veoma jednostavan za korišćenje, veoma brz i praktičan.
+            </p>
         </div>
     </div>
 </section>
 <section class="benefits_section">
     <div class="wrapper">
-        <h2>Почему <span>Чистящая щетка Turbo Scrub 360</span>?</h2>
+        <h2>ZAŠTO<span> TURBO CLEAN</span>?</h2>
         <div class="benefits_list">
             <div class="benefit_item">
-                <img src="{{ asset('/') }}dailysharkFiles/turboclean/benefits__benefit1_image.jpg" alt="Чистящая щетка Turbo Scrub 360">
-                <h4>Turbo Scrub 360 очень прост в использовании, очень быстрый и интуитивно понятный для сборки и немедленного использования.</h4>
+                <img src="{{ asset('/') }}dailysharkFiles/turboclean/benefits__benefit1_image.jpg" alt="Turbo Clean">
+                <h4>TURBO CLEAN je veoma jednostavan za korišćenje, veoma brz i odličan za sve koji ne žele ili ne mogu da troše mnogo snage na čišćenje.</h4>
             </div>
             <div class="benefit_item">
-                <img src="{{ asset('/') }}dailysharkFiles/turboclean/benefits__benefit2_image.jpg" alt="Чистящая щетка Turbo Scrub 360">
-                <h4>Угловая щетка удаляет грязь с самых сложных мест, где обычная щетка не смогла бы.</h4>
+                <img src="{{ asset('/') }}dailysharkFiles/turboclean/benefits__benefit2_image.jpg" alt="Turbo Clean">
+                <h4>Ugaona četka uklanja prljavštinu sa najtežih mesta gde obična četka ne može da priđe.</h4>
             </div>
             <div class="benefit_item">
-                <img src="{{ asset('/') }}dailysharkFiles/turboclean/benefits__benefit3_image.jpg" alt="Чистящая щетка Turbo Scrub 360">
-                <h4>С удлинителем ручки, вы достигаете даже самых сложных мест, для чистки сверху донизу, без растягивания или сидения на корточках! </h4>
+                <img src="{{ asset('/') }}dailysharkFiles/turboclean/benefits__benefit3_image.jpg" alt="Turbo Clean">
+                <h4>Sa produžetkom ručke stižete i do najtežih mesta za čišćenje, od vrha do dna bez istezanja i čučnjeva!</h4>
             </div>
             <div class="benefit_item">
-                <img src="{{ asset('/') }}dailysharkFiles/turboclean/benefits__benefit4_image.jpg" alt="Чистящая щетка Turbo Scrub 360">
-                <h4>Его мощная плоская щетка всегда готова устранить самую жесткую грязь, такую ​​как жир, который накапливается в духовке!</h4>
+                <img src="{{ asset('/') }}dailysharkFiles/turboclean/benefits__benefit4_image.jpg" alt="Turbo Clean">
+                <h4>Njegova moćna ravna četka je uvek spremna da ukloni najtvrdokorniju prljavštinu poput masnoće koja se nakuplja u rerni!</h4>
             </div>
         </div>
     </div>
 </section>
 <section class="info_section">
     <div class="wrapper clearfix">
-        <h2><span>Техническое описание:</span></h2>
-        <img src="{{ asset('/') }}dailysharkFiles/turboclean/info__image.jpg" alt="Чистящая щетка Turbo Scrub 360">
+        <h2><span>TEHNIČKE KARAKTERISTIKE:</span></h2>
+        <img src="{{ asset('/') }}dailysharkFiles/turboclean/info__image.jpg" alt="Turbo Clean">
         <div class="content_block scroll">
             <div class="text_block">
                 <ul style="font-size: 26px;line-height: 40px;">
-                    <li>Производитель: Turbo Scrub</li>
-                    <li>Материал: ABS и силикон</li>
-                    <li>Цвет: белый и черный</li>
-                    <li>Рабочее время: около 60 минут.</li>
-                    <li>Вращение под углом 120 градусов.</li>
-                    <li>Может использоваться для работы с мылом, плесенью, грязью.</li>
-                    <li>360 оборотов в минуту.</li>
+                    <li>Materijal: ABS i silikon</li>
+                    <li>Rotacija pod uglom od 120 stepeni</li>
+                    <li>Može da se koristi za rad sa sapunom i svom kupatilskom i kuhinjskom hemijom</li>
+                    <li>360 rotacija u minuti</li>
                 </ul>
             </div>
             <div class="price_block clearfix">
                 <div class="price_item old">
-                    <div class="text">Обычная цена:</div>
+                    <div class="text">Redovna cena:</div>
                     <div class="value">
                         {{ $prices[1]['originalPrice'] }}
                         {{ $prices[1]['currency'] }}
                     </div>
                 </div>
                 <div class="price_item new">
-                    <div class="text">Цена сегодня:</div>
+                    <div class="text">Cena sada:</div>
                     <div class="value">
                         {{ $prices[1]['amount'] }}
                         {{ $prices[1]['currency'] }}
@@ -143,36 +147,36 @@
 </section>
 <section class="set_section">
     <div class="wrapper">
-        <h2><span>Turbo Scrub 360</span> можно использовать везде</h2>
+        <h2><span>TURBO CLEAN</span> SE MOŽE KORISTITI SVUDA</h2>
         <div class="set_list clearfix">
             <div class="set_item clearfix">
                 <div class="text_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image1.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image1.jpg" alt="Turbo Clean">
                 </div>
             </div>
             <div class="set_item clearfix">
                 <div class="text_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image2.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image2.jpg" alt="Turbo Clean">
                 </div>
             </div>
             <div class="set_item clearfix">
                 <div class="text_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image3.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image3.jpg" alt="Turbo Clean">
                 </div>
             </div>
             <div class="set_item clearfix">
                 <div class="text_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image4.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image4.jpg" alt="Turbo Clean">
                 </div>
             </div>
             <div class="set_item clearfix">
                 <div class="text_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image5.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image5.jpg" alt="Turbo Clean">
                 </div>
             </div>
             <div class="set_item clearfix">
                 <div class="text_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image6.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/set_image6.jpg" alt="Turbo Clean">
                 </div>
             </div>
         </div>
@@ -181,43 +185,47 @@
 <section class="opinion_section">
     <div class="wrapper">
         <div class="content_block">
-            <h2>Чистящая щетка <span>Turbo Scrub 360</span></h2>
-            <img src="{{ asset('/') }}dailysharkFiles/turboclean/opinion__image.jpg" alt="Чистящая щетка Turbo Scrub 360">
+            <h2>ČETKA ZA ČIŠĆENJE <br><span>TURBO CLEAN</span></h2>
+            <img src="{{ asset('/') }}dailysharkFiles/turboclean/opinion__image.jpg" alt="Turbo Clean">
             <div class="text_block">
-                <p>Turbo Scrub 360 является перезаряжаемым, беспроводным и полностью водонепроницаемым. Для очистки, там где вам это нужно больше всего, даже в саду, ванной или бассейне! И вы можете быть спокойны, потому что мягкие щетины не царапают поверхность! В результате получается безупречная чистота, быстро и без усилий! </p>
+                <p>TURBO CLEAN je punjivi, bežični uređaj i potpuno je vodootporan. Za čišćenje tamo gde je najpotrebnije, čak i u bašti, kupatilu ili bazenu!<br><br>
+                    <b>Rezultat je besprekorna čistoća, brzo i bez napora!</b></p>
             </div>
         </div>
     </div>
 </section>
 <section class="reviews_section">
     <div class="wrapper">
-        <h2><span>Отзывы</span> наших покупателей</h2>
+        <h2><span>UTISCI</span> NAŠIH KUPACA</h2>
         <div class="reviews_list">
             <div class="review_item clearfix">
                 <div class="image_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/reviews__review1_photo.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/reviews__review1_photo.jpg" alt="Turbo Clean">
                 </div>
                 <div class="text_block">
-                    <div class="author_name">Анна Кравцова</div>
-                    <p>Качественный и продуманный очиститель! Ванную чистит отлично. Очень удобный удлинитель ручки, уменьшает затраты времени и сил.</p>
+                    <div class="author_name">ANA KRSTOVIĆ</div>
+                    <p>Visokokvalitetno i korisno sredstvo za čišćenje! Kupatilo odlično riba. Veoma zgodna ručka za produžetak koja štedi vreme i trud.</p>
                 </div>
             </div>
             <div class="review_item clearfix">
                 <div class="image_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/reviews__review2_photo.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/reviews__review2_photo.jpg" alt="Turbo Clean">
                 </div>
                 <div class="text_block">
-                    <div class="author_name">Иван Морисов</div>
-                    <p>Изучив предложения различных производителей, выбрал для себя Чистящую щетку Turbo Scrub 360. Во-первых - компактность. Можно разделить все части, и удобно сложить в полку. Во-вторых - удобный удлинитель ручки, для быстрой и удобной чистки. Очиститель сделан качественно, пластик хороший, ручка удобная.</p>
+                    <div class="author_name">IVAN MARKOVIĆ</div>
+                    <p>Nakon što sam pažljivo pogledao ponudu odlučio sam da poručim ovu četku. Odlična je i preporučujem je. Držim ribarnicu i zbog inspekcije mora sve da bude čisto.
+                        Sa ovom četkom oribam pločice i radne površine bez ikakvih problema, temeljno i za duplo kraće vreme nego ranije što sam radio.
+                        Kvalitetna je i mogu samo da kažem da je ovo najbolja kupovina preko interneta do sada.</p>
                 </div>
             </div>
             <div class="review_item clearfix">
                 <div class="image_block">
-                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/reviews__review3_photo.jpg" alt="Чистящая щетка Turbo Scrub 360">
+                    <img src="{{ asset('/') }}dailysharkFiles/turboclean/reviews__review3_photo.jpg" alt="Turbo Clean">
                 </div>
                 <div class="text_block">
-                    <div class="author_name">Ольга Телегина</div>
-                    <p>Очиститель очень удобный. Удобнее, чем я себе представляла. Также отлично, что он крепкий, добротное и действительно неплохое качество. В общем, очень рекомендую, особенно тем, у кого мало времени на уборку. Вещь в хозяйстве необходимая.</p>
+                    <div class="author_name">OLGA AVRAMOVIĆ</div>
+                    <p>Uh, ovo je odlično čoveče. Bolje je nego što sam zamišljala. Kada ribam kupatilo i kuhinju nikada nisam mogla toliko jako da pritisnem i skinem kamenac.
+                        Sa ovom četkom uspela sam da dovedem sve do toga da se sija. Toplo preporučujem kupovinu, TURBO CLEAN četka je neophodna u svakom domaćinstvu.</p>
                 </div>
             </div>
         </div>
@@ -225,43 +233,43 @@
 </section>
 <section class="order_steps_section">
     <div class="wrapper">
-        <h2>Доставка и оплата</h2>
+        <h2>DOSTAVA I PLAĆANJE</h2>
         <div class="steps_list clearfix">
             <div class="step_item">
-                <img src="{{ asset('/') }}dailysharkFiles/turboclean/order_steps__step1_image.jpg" alt="Чистящая щетка Turbo Scrub 360">
-                <h4>Заявка</h4>
-                <p>Вы оставляете заявку на нашем сайте</p>
+                <img src="{{ asset('/') }}dailysharkFiles/turboclean/order_steps__step1_image.jpg" alt="Turbo Clean">
+                <h4>PORUČIVANJE</h4>
+                <p>Ostavite podatke za dostavu na našem sajtu</p>
             </div>
             <div class="step_item">
-                <img src="{{ asset('/') }}dailysharkFiles/turboclean/order_steps__step2_image.jpg" alt="Чистящая щетка Turbo Scrub 360">
-                <h4>Отправка</h4>
-                <p>Менеджер перезванивает для оформления заказа</p>
+                <img src="{{ asset('/') }}dailysharkFiles/turboclean/order_steps__step2_image.jpg" alt="Turbo Clean">
+                <h4>SLANJE</h4>
+                <p>Kontaktiraćemo vas da potvrdimo porudžbinu i poslati je u roku od 1-2 radna dana</p>
             </div>
             <div class="step_item">
-                <img src="{{ asset('/') }}dailysharkFiles/turboclean/order_steps__step3_image.jpg" alt="Чистящая щетка Turbo Scrub 360">
-                <h4>Получение</h4>
-                <p>Доставка по СНГ осуществляется почтой от 7 рабочих дней в зависимости от региона</p>
+                <img src="{{ asset('/') }}dailysharkFiles/turboclean/order_steps__step3_image.jpg" alt="Turbo Clean">
+                <h4>PLAĆANJE</h4>
+                <p>Pošiljku plaćate kuriru brze pošte po preuzimanju</p>
             </div>
         </div>
     </div>
 </section>
 <section class="order_section">
     <div class="wrapper">
-        <h4 class="subtitle">Чистящая щетка</h4>
-        <h3 class="main_title">Turbo Scrub 360</h3>
-        <p class="description">тяжелая уборка одним нажатием кнопки</p>
-        <div class="discount">Акция! <span>50%</span> скидка</div>
+        <h4 class="subtitle">ČETKA ZA ČIŠĆENJE</h4>
+        <h3 class="main_title">TURBO CLEAN</h3>
+        <p class="description">ČIŠĆENJE PRITISKOM NA DUGME</p>
+        <div class="discount">Akcija! <span>40%</span> popusta</div>
         <div class="form_block">
             <div class="price_block clearfix">
                 <div class="price_item old">
-                    <div class="text">Обычная цена:</div>
+                    <div class="text">Redovna cena:</div>
                     <div class="value">
                         {{ $prices[1]['originalPrice'] }}
                         {{ $prices[1]['currency'] }}
                     </div>
                 </div>
                 <div class="price_item new">
-                    <div class="text">Цена сегодня:</div>
+                    <div class="text">Cena sada:</div>
                     <div class="value">
                         {{ $prices[1]['amount'] }}
                         {{ $prices[1]['currency'] }}
@@ -290,5 +298,6 @@
         });
     });
 </script>
+@include('components.pixel_footer')
 </body>
 </html>
