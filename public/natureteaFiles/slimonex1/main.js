b5fc18a19bea8ea2cd0881dd2f20882ab02e4434
filipnestorovic <filipnestor95@gmail.com>
2023-02-20 11:53:38 +0100

    Flasteri - ChangeQuantity v2

--- public/natureteaFiles/slimonex1/main.js
+++ public/natureteaFiles/slimonex1/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+    let initialQuantity = $('.change-package-selector2').val();
+    $('.item-gift').hide();
+    $('.' + initialQuantity + '_pack').show();
+    $('.change-package-selector2 [value="' + initialQuantity + '"]').prop("selected", true);
+
     $('.change-package-selector2').on('change', (function() {
         $('.item-gift').hide();
         $('.' + $(this).val() + '_pack').show();
