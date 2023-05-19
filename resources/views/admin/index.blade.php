6dc5d4c7d0fe1f9f4a61c79c517860138a2b399a
filipnestorovic <filipnestor95@gmail.com>
2023-05-19 10:42:44 +0200

    AdminIndex v2

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,16 +6,16 @@
             <li class="breadcrumb-item active" aria-current="page"></li>
         </ol>
     </nav>
-    <div class="card-body">
-        <div class="col-xl-12 col-md-12">
-            Country:
-            <select name="countryDdl" id="countryDdl" data-toggle="dropdown">
-                @foreach($countries as $country)
-                    <option value="{{ $country->id_country }}">{{ $country->country_name }}</option>
-                @endforeach
-            </select>
-        </div>
-    </div>
+{{--    <div class="card-body">--}}
+{{--        <div class="col-xl-12 col-md-12">--}}
+{{--            Country:--}}
+{{--            <select name="countryDdl" id="countryDdl" data-toggle="dropdown">--}}
+{{--                @foreach($countries as $country)--}}
+{{--                    <option value="{{ $country->id_country }}">{{ $country->country_name }}</option>--}}
+{{--                @endforeach--}}
+{{--            </select>--}}
+{{--        </div>--}}
+{{--    </div>--}}
     <div class="mb-4 wow fadeIn">
         <div class="card-body d-sm-flex justify-content-between">
             <div class="col-xl-12 col-md-12">
@@ -35,29 +35,28 @@
     </style>
 @endsection
 @section('scripts')
-    <script>
-        $(document).ready(function () {
-
-            function fetch_data() {
-                let country_id = $('#countryDdl').val();
-                let currentPage = 1;
-                $.ajax({
-                    url: baseURL + "ajaxData/getActiveVariationsStatistic?page=" + currentPage,
-                    data: {country_id:country_id},
-                    success: function (data) {
-                        console.log(data);
-                        $('#tableStatisticAjax').html('');
-                        $('#tableStatisticAjax').html(data);
-                    },
-                    error: function (req, err) {
-                        $('#errorMessageHeader').html('Error on getting variations list');
-                        $('#errorMessageHeader').slideDown();
-                    }
-                });
-            }
-            fetch_data();
-            // setInterval(fetch_data, 20000);
-            $('#countryDdl').change(fetch_data);
-        });
-    </script>
+{{--    <script>--}}
+{{--        $(document).ready(function () {--}}
+{{--            function fetch_data() {--}}
+{{--                let country_id = $('#countryDdl').val();--}}
+{{--                let currentPage = 1;--}}
+{{--                $.ajax({--}}
+{{--                    url: baseURL + "ajaxData/getActiveVariationsStatistic?page=" + currentPage,--}}
+{{--                    data: {country_id:country_id},--}}
+{{--                    success: function (data) {--}}
+{{--                        console.log(data);--}}
+{{--                        $('#tableStatisticAjax').html('');--}}
+{{--                        $('#tableStatisticAjax').html(data);--}}
+{{--                    },--}}
+{{--                    error: function (req, err) {--}}
+{{--                        $('#errorMessageHeader').html('Error on getting variations list');--}}
+{{--                        $('#errorMessageHeader').slideDown();--}}
+{{--                    }--}}
+{{--                });--}}
+{{--            }--}}
+{{--            fetch_data();--}}
+{{--            // setInterval(fetch_data, 20000);--}}
+{{--            $('#countryDdl').change(fetch_data);--}}
+{{--        });--}}
+{{--    </script>--}}
 @endsection
